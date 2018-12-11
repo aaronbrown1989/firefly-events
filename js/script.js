bc8cddeb87755e41f76856df40c370034eefd3ab
Hoyt Reid <hoytsreid@gmail.com>
2018-12-11 12:10:43 +0000

    style carousel for desktop

--- js/script.js
+++ js/script.js
@@ -10,9 +10,46 @@ $(document).ready(function(){
         breakpoint: 600,
         settings: {
           centerMode: true,
-          centerPadding: '100px'
+          centerPadding: '100px',
+          swipeToSlide: true,
         }
       },
+      {
+        breakpoint: 900,
+        settings: {
+          arrows: true,
+          prevArrow: '<button type="button" class="slick-prev"><ion-icon name="ios-arrow-dropleft-circle"></ion-icon></button>',
+          nextArrow: '<button type="button" class="slick-next"><ion-icon name="ios-arrow-dropright-circle"></ion-icon></button>',
+          centerMode: true,
+          centerPadding: '200px',
+          dots: false,
+          swipeToSlide: false
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          arrows: true,
+          prevArrow: '<button type="button" class="slick-prev"><ion-icon name="ios-arrow-dropleft-circle"></ion-icon></button>',
+          nextArrow: '<button type="button" class="slick-next"><ion-icon name="ios-arrow-dropright-circle"></ion-icon></button>',
+          centerMode: true,
+          centerPadding: '300px',
+          dots: false,
+          swipeToSlide: false
+        }
+      },
+      {
+        breakpoint: 1600,
+        settings: {
+          arrows: true,
+          prevArrow: '<button type="button" class="slick-prev"><ion-icon name="ios-arrow-dropleft-circle"></ion-icon></button>',
+          nextArrow: '<button type="button" class="slick-next"><ion-icon name="ios-arrow-dropright-circle"></ion-icon></button>',
+          centerMode: true,
+          centerPadding: '500px',
+          dots: false,
+          swipeToSlide: false
+        }
+      }
     ]
   });
 });
