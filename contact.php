111f337adcd90da3964af8ed7b77f8f58d6901fa
Hoyt Reid <hoytsreid@gmail.com>
2018-12-13 16:23:17 +0000

    add content and basic styling to contact page

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php include 'header.php';?>
+  <main>
+    <h1>Contact Us</h1>
+    <p class="tagline">If you want to hire us for your event or just touch base, fill in the form below or give us a call!</p>
+    <form id="contact-form">
+      <label>Name</label>
+      <input type="text" />
+      <label>Email</label>
+      <input type="email" />
+      <label>Message</label>
+      <textarea></textarea>
+      <button class="button" type="submit">Send</button>
+    </form>
+
+    <ul class="contact-info">
+      <li><ion-icon name="ios-home"></ion-icon> Penfold Drive, Gateway 11 Business Park, Wymondham, Norfolk, NR18 0WZ</li>
+      <li><ion-icon name="ios-call"></ion-icon> 01234-456-789</li>
+      <li><ion-icon name="ios-mail"></ion-icon> info@ffevents.co.uk</li>
+    </ul>
+  </main>
+<?php include 'footer.php';?>
