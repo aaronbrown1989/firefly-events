a1c031e0bf1e67e3b2b94a33db7d968d2815d4a2
Hoyt Reid <hoytsreid@gmail.com>
2018-12-14 08:25:05 +0000

    add map to contact page

--- contact.php
+++ contact.php
@@ -13,6 +13,9 @@
     </form>
 
     <div class="contact-info-container">
+      <div class="map-container">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2424.5319405098426!2d1.1365683160005726!3d52.57807523967587!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d9ddadc72efd55%3A0xb0b2b7657eba28c7!2sGateway+11+Business+Park!5e0!3m2!1sen!2suk!4v1544719853382" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
       <ul class="contact-info">
         <li><ion-icon name="ios-home"></ion-icon> <span>Penfold Drive, Gateway 11 Business Park, Wymondham, Norfolk, NR18 0WZ</span></li>
         <li><ion-icon name="ios-call"></ion-icon> <span>01234-456-789</span></li>
