0cfe0cfccd900f6e8f151dd8d6c0c4e085083774
Hoyt Reid <hoytsreid@gmail.com>
2018-12-18 10:22:53 +0000

    add code for testimonials page

---
+++ testimonials.php
@@ -0,0 +1,33 @@
+<?php include 'header.php';?>
+
+<main>
+  <h1>Testimonials</h1>
+  <section id="testimonials">
+    <div class="testimony-container">
+      <img class="testimony-logo" src="img/logo/partners/helm-logo.png" alt="Helms Logo">
+      <p class="testimony">FireFly events did a great job setting up and helping us run one of our first venues. we will definitely be useing them again.</p>
+    </div>
+    <div class="testimony-container">
+      <img class="testimony-logo" src="img/logo/partners/film-festival-logo.png" alt="Film Festival Logo">
+      <p class="testimony">FireFly events did a great job setting up and helping us run one of our first venues. we will definitely be useing them again.</p>
+    </div>
+    <div class="testimony-container">
+      <img class="testimony-logo" src="img/logo/partners/vfestival-logo.png" alt="V Festival Logo">
+      <p class="testimony">FireFly events did a great job setting up and helping us run one of our first venues. we will definitely be useing them again.</p>
+    </div>
+    <div class="testimony-container">
+      <img class="testimony-logo" src="img/logo/partners/helm-logo.png" alt="Helms Logo">
+      <p class="testimony">FireFly events did a great job setting up and helping us run one of our first venues. we will definitely be useing them again.</p>
+    </div>
+    <div class="testimony-container">
+      <img class="testimony-logo" src="img/logo/partners/film-festival-logo.png" alt="Film Festival Logo">
+      <p class="testimony">FireFly events did a great job setting up and helping us run one of our first venues. we will definitely be useing them again.</p>
+    </div>
+    <div class="testimony-container">
+      <img class="testimony-logo" src="img/logo/partners/vfestival-logo.png" alt="V Festival Logo">
+      <p class="testimony">FireFly events did a great job setting up and helping us run one of our first venues. we will definitely be useing them again.</p>
+    </div>
+  </section>
+</main>
+
+<?php include 'footer.php';?>
