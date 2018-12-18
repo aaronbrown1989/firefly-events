86d2455dc91ac8076fef886ea34073acbbef96b0
Hoyt Reid <hoytsreid@gmail.com>
2018-12-18 10:49:40 +0000

    add responsive styles to testimonials page

--- testimonials.php
+++ testimonials.php
@@ -1,31 +1,43 @@
 <?php include 'header.php';?>
 
-<main>
+<main id="testimonials-page">
   <h1>Testimonials</h1>
   <section id="testimonials">
     <div class="testimony-container">
       <img class="testimony-logo" src="img/logo/partners/helm-logo.png" alt="Helms Logo">
       <p class="testimony">FireFly events did a great job setting up and helping us run one of our first venues. we will definitely be useing them again.</p>
+      <p class="attribution"><span class="attribution-name">Suzanne Robertson</span><br />
+        <span class="attribution-title">Director</span></p>
     </div>
     <div class="testimony-container">
       <img class="testimony-logo" src="img/logo/partners/film-festival-logo.png" alt="Film Festival Logo">
       <p class="testimony">FireFly events did a great job setting up and helping us run one of our first venues. we will definitely be useing them again.</p>
+      <p class="attribution"><span class="attribution-name">Joise Cruise</span><br />
+        <span class="attribution-title">Sales Director</span></p>
     </div>
     <div class="testimony-container">
       <img class="testimony-logo" src="img/logo/partners/vfestival-logo.png" alt="V Festival Logo">
       <p class="testimony">FireFly events did a great job setting up and helping us run one of our first venues. we will definitely be useing them again.</p>
+      <p class="attribution"><span class="attribution-name">Dave Jones</span><br />
+        <span class="attribution-title">Project Manager</span></p>
     </div>
     <div class="testimony-container">
       <img class="testimony-logo" src="img/logo/partners/helm-logo.png" alt="Helms Logo">
       <p class="testimony">FireFly events did a great job setting up and helping us run one of our first venues. we will definitely be useing them again.</p>
+      <p class="attribution"><span class="attribution-name">Sheena Lister</span><br />
+        <span class="attribution-title">Operations Manager</span></p>
     </div>
     <div class="testimony-container">
       <img class="testimony-logo" src="img/logo/partners/film-festival-logo.png" alt="Film Festival Logo">
       <p class="testimony">FireFly events did a great job setting up and helping us run one of our first venues. we will definitely be useing them again.</p>
+      <p class="attribution"><span class="attribution-name">Ross Grindle</span><br />
+        <span class="attribution-title">Head of Events</span></p>
     </div>
     <div class="testimony-container">
       <img class="testimony-logo" src="img/logo/partners/vfestival-logo.png" alt="V Festival Logo">
       <p class="testimony">FireFly events did a great job setting up and helping us run one of our first venues. we will definitely be useing them again.</p>
+      <p class="attribution"><span class="attribution-name">Hannah Greenway</span><br />
+        <span class="attribution-title">Director</span></p>
     </div>
   </section>
 </main>
