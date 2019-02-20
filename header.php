afe362ba39ee673c4063f13314b9fd1e8cb8f9ac
Hoyt Reid <hoytsreid@gmail.com>
2019-02-20 14:40:43 +0000

    add updated navigation to header file

--- header.php
+++ header.php
@@ -21,13 +21,14 @@
         </div>
       </div>
       <div id="desk-header">
-        <img id="desk-logo" src="img/logo/logo-light.png" />
+        <img id="desk-logo" src="./img/logo/logo-light.png" />
         <ul id="desk-nav">
-          <li>Home</li>
-          <li>What's on</li>
-          <li>About us</li>
-          <li>Our Events</li>
-          <li>Contact Us</li>
+          <li><a href="index.php" class="home-link">Home</a></li>
+          <li><a href="whatson.php" class="whatson-link">What's on</a></li>
+          <li><a href="about.php" class="about-link">About us</a></li>
+          <li><a href="services.php" class="services-link">Services</a></li>
+          <li><a href="events.php" class="events-link">Our Events</a></li>
+          <li><a href="contact.php" class="contact-link">Contact Us</a></li>
         </ul>
       </div>
     </header>
