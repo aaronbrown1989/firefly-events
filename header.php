319bbd922ebfad4217d8446f15fd174706931e3d
Hoyt Reid <hoytsreid@gmail.com>
2019-02-26 15:17:46 +0000

    add html for mobile menu

--- header.php
+++ header.php
@@ -11,7 +11,14 @@
   </head>
   <body>
     <div id="mobile-menu">
-      [Mobile menu test]
+      <ul>
+        <li><a href="index.php" class="home-link">Home</a></li>
+        <li><a href="whatson.php" class="whatson-link">What's on</a></li>
+        <li><a href="about.php" class="about-link">About us</a></li>
+        <li><a href="services.php" class="services-link">Services</a></li>
+        <li><a href="events.php" class="events-link">Our Events</a></li>
+        <li><a href="contact.php" class="contact-link">Contact Us</a></li>
+      </ul>
     </div>
     <div id="page-content">
       <header>
