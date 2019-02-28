2383a8402216a420460e98bd734feedfaaa426e2
Hoyt Reid <hoytsreid@gmail.com>
2019-02-28 13:49:25 +0000

    convert article page into php

---
+++ article.php
@@ -0,0 +1,71 @@
+<?php include 'header.php';?>
+
+  <div class="news-article-container">
+    <!-- this is the main news article  -->
+    <div class="newsArticle">
+      <div class="head">
+        <h2 class="name">News Artice : "Name"</h2>
+      </div>
+      <div class="box">
+      <div class="info">
+        news/ suport
+      </div>
+      <i class="fab fa-facebook fa-2x"></i>
+      <i class="fab fa-twitter-square fa-2x"></i>
+    </div>
+      <div class="body">
+        <h4 class="sub-title">information of the news atricle</h4>
+        <p class="font">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat arcu vitae sapien suscipit, vitae cursus ipsum ultrices. Vestibulum in congue nibh, egestas aliquet augue. Curabitur viverra consequat metus a faucibus. Morbi dignissim vitae lectus eget euismod. Vestibulum nec hendrerit purus. Vestibulum libero purus, rutrum quis accumsan eu, condimentum in urna. Morbi et ultricies orci. In hac habitasse platea dictumst. Etiam velit justo, maximus ut vehicula fringilla, laoreet ac orci. Nulla facilisi. Aliquam erat volutpat.
+        <br>
+        <br>
+        Curabitur turpis quam, ornare sed elit eget, rhoncus vulputate turpis. Fusce tempor euismod sem eget lacinia. Fusce erat orci, pretium sit amet ligula nec, fringilla sagittis tortor. Quisque ac nisl ante. Aliquam non sem placerat, sollicitudin lorem non, eleifend ligula. Curabitur risus nisl, suscipit ac ligula sit amet, semper aliquet augue. Praesent consectetur lacus sed leo vehicula dapibus. Mauris fermentum turpis eget pellentesque eleifend. Nam ut libero aliquet, aliquam dolor vitae, bibendum massa. Nunc sollicitudin erat et libero cursus accumsan. Sed at lobortis diam, lacinia consectetur mi. Etiam pharetra dui nisi.
+        <br>
+        <br>
+        Suspendisse vitae cursus quam. Quisque sed lacus leo. Vivamus semper tempus tincidunt. Phasellus suscipit molestie orci. Praesent bibendum eleifend pulvinar. Nunc eget consectetur lectus. Aenean pellentesque, dui at luctus vestibulum, velit ex consectetur orci, non ornare mauris mauris id mi.
+        <br>
+        <br>
+        Aliquam erat volutpat. Donec aliquam ultrices ipsum, viverra sollicitudin lectus cursus faucibus. Fusce a pharetra purus. Pellentesque at leo sit amet metus dignissim sodales. Mauris pellentesque molestie commodo. Nunc facilisis orci nec libero efficitur, et porta magna auctor. Donec pellentesque placerat lacinia. Mauris sed ipsum id lorem aliquet ornare nec id purus. Nunc pellentesque orci eu massa consequat varius. Fusce nec massa sapien. Aliquam nibh justo, venenatis a suscipit id, ultricies in dolor. Nullam ullamcorper tincidunt euismod.
+        </p>
+        <button type="button" name="button" class="back-button button">BACK</button>
+        <div class="date_and_who_update">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5Nxm83FzblZ8u_qy4b7mfLD_TylswLTRh8kpP74swqDe1pPnB" alt="picture" class="profile-pic">
+          <div class="location">
+            <p class="img-user">Posted by Mic</p>
+            <p class="img-date">18th December 2018</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!---->
+    <!-- side articles  -->
+    <div class="min-container">
+      <div class="side-article">
+        <div class="info-side">
+          news
+        </div>
+        <img src="https://us.123rf.com/450wm/yamabikay/yamabikay1602/yamabikay160200385/52809799-light-blue-paper-texture-blank-background-for-template-.jpg?ver=6" alt="test" class="img-size">
+        <h4 class="foo">Lorem ipsum dolor sit amet, consectetur.</h4>
+        <button type="button" name="button" class="bun button">Read More</button>
+      </div>
+      <div class="side-article">
+        <div class="info-side-dif">
+          news
+        </div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMiJo2vtANXOdr4DNg1WayV_r5KkUrQff0-NO3WcI1cwzBczdt" alt="test" class="img-size">
+        <h4 class="foo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tellus ex ...</h4>
+        <button type="button" name="button" class="bun button">Read More</button>
+      </div>
+      <div class="side-article">
+        <div class="info-side">
+          news
+        </div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdzNQbViOX6TM68tCkRBJVe7vtAwtbFj64tByEilRY7aDALb1W" alt="test" class="img-size">
+        <h4 class="foo">Lorem ipsum dolor sit amet, consectetur adipiscing elit phasellus ...</h4>
+        <button type="button" name="button" class="bun button">Read More</button>
+      </div>
+    </div>
+    <!--  -->
+  </div>
+
+<?php include 'footer.php';?>
