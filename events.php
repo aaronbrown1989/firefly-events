06b51743761f99ced0955f8135664429a829eecc
Hoyt Reid <hoytsreid@gmail.com>
2019-03-12 12:10:59 +0000

    convert case studies page into php

---
+++ events.php
@@ -0,0 +1,42 @@
+<?php include 'header.php';?>
+  <main>
+    <div class="case-headcontainer">
+      <h1 class="case-main-header">Our Events</h1>
+      <p class="case-main-info">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec volutpat lacus, in condimentum risus. Cras at posuere massa, in luctus ex. Sed sagittis arcu nec mollis viverra. Aenean ut ante iaculis arcu luctus consequat. Cras mauris velit, rhoncus semper nunc eu, tincidunt efficitur leo. Duis mattis, sapien non fermentum pretium, nisi magna tristique tortor, a molestie dolor augue non nulla. Vestibulum aliquet accumsan velit ac luctus. Sed consequat sit amet nisl fringilla pellentesque. Aenean vitae facilisis sem.
+      </p>
+    </div>
+    <div class="casestudies-container">
+      <div class="case-studycontainer">
+        <div class="article-image">
+          <img class="case-img" alt="Case Study 1" src="img\case-studies\case-1.png">
+        </div>
+        <h2 class="articleheader">Case Study 1</h2>
+        <p class="case-studytext">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec laoreet nunc id convallis ullamcorper. Nam sed lorem metus. Cras pretium, est at tincidunt scelerisque, nisi libero lobortis sapien, et tempus velit tellus eget felis. Curabitur scelerisque laoreet odio, eget cursus quam. Phasellus eget turpis arcu. Vivamus tempor lectus id magna mollis, in semper est luctus. Nullam facilisis auctor porttitor...
+        </p>
+        <a href="article.php" class="button case-button">Read More...</a>
+      </div>
+      <div class="case-studycontainer">
+        <div class="article-image">
+          <img class="case-img" alt="Case Study 2" src="img\case-studies\case-2.png">
+        </div>
+        <h2 class="articleheader">Case Study 2</h2>
+        <p class="case-studytext">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget massa sem. Nunc imperdiet ut sem non pulvinar. Vivamus non lacinia nisi. Cras ultricies, quam vitae commodo pharetra, leo dui tincidunt justo, eu elementum lectus est et lacus. Aliquam rutrum non augue eget faucibus. Sed congue pulvinar velit, vel dapibus mi malesuada eget. Aenean ullamcorper sapien sit amet elit hendrerit venenatis. Donec eu molestie turpis...
+        </p>
+        <a href="article.php" class="button case-button">Read More...</a>
+      </div>
+      <div class="case-studycontainer">
+        <div class="article-image">
+          <img class="case-img" alt="Case Study 3" src="img\case-studies\case-3.png">
+        </div>
+        <h2 class="articleheader">Case Study 3</h2>
+        <p class="case-studytext">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque finibus ullamcorper tristique. Pellentesque sit amet efficitur libero. Integer porttitor porttitor quam, eu pharetra risus semper nec. Aenean at volutpat elit, sed tristique lectus. Phasellus interdum blandit ornare. Praesent rhoncus volutpat lectus. Sed at consequat risus. Integer sapien est, feugiat eget lacinia eu, pretium ut ligula. Nam posuere a augue eu tempor...
+        </p>
+        <a href="article.php" class="button case-button">Read More...</a>
+      </div>
+    </div>
+  </main>
+<?php include 'footer.php';?>
