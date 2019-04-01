5b59e162a959b3d1f6ca0fcce007b0e8aa56f6f2
aaronbrown1989 <45995788+aaronbrown1989@users.noreply.github.com>
2019-04-01 13:57:11 +0100

    fixed the menu this time.

--- header.php
+++ header.php
@@ -35,8 +35,8 @@
           <a class="logo-link" href="index.php"><img id="desk-logo" src="./img/logo/logo-light.png" /></a>
           <ul id="desk-nav">
             <li><a href="index.php" class="home-link">Home</a></li>
-            <li><a href="whatson.php" class="whatson-link">What's on</a></li>
-            <li><a href="about.php" class="about-link">About us</a></li>
+            <li><a href="whatson.php" class="whatson-link">What's On</a></li>
+            <li><a href="about.php" class="about-link">About Us</a></li>
             <li><a href="services.php" class="services-link">Services</a></li>
             <li><a href="events.php" class="events-link">Our Events</a></li>
             <li><a href="contact.php" class="contact-link">Contact Us</a></li>
