c1dbe3dfc1b604b24a45265ed3c16fa5d8e8d8ab
Tim Loukes <tim.loukes@netmatters.local>
2019-04-08 16:12:57 +0100

    Changed h4 to h3 on "about us" page.

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
   <h1>About Us</h1>
   <section class="about-background-image"  id="about-page">
     <div>
-      <h4>Who we are</h4>
+      <h3>Who we are</h3>
       <p>Firefly is an event management company based out of Norwich. Our experienced and well adapted team perform a number of services for a wide range of clients.</p>
       <p>Spanning everything from inital ideasm hosting and even advertisement aspects.</p>
     </div>
