460069d67b683965088178ad8f82d4015bc9caff
Ross <rbunn1997@gmail.com>
2019-05-02 15:56:51 +0100

    I have made it so that the calender dosnt creat any errors when on pages that dosnt have the calender

--- js/script.js
+++ js/script.js
@@ -134,7 +134,8 @@ function resizeFbPlugin() {
 }
 
 // calendar module
-
+if(document.getElementById("calendar-body"))
+{
 let today = new Date();
 let currentMonth = today.getMonth();
 let currentYear = today.getFullYear();
@@ -165,51 +166,53 @@ function jump() {
     showCalendar(currentMonth, currentYear);
 }
 
-function showCalendar(month, year) {
+  function showCalendar(month, year) {
 
-  let firstDay = (new Date(year, month)).getDay();
-  let daysInMonth = 32 - new Date(year, month, 32).getDate();
-
-  let tbl = document.getElementById("calendar-body"); // body of the calendar
-
-  // clearing all previous cells
-  tbl.innerHTML = "";
-
-  // filing data about month and in the page via DOM.
-  monthAndYear.innerHTML = months[month] + " " + year;
-  selectYear.value = year;
-  selectMonth.value = month;
-
-  // creating all cells
-  let date = 1;
-  for (let i = 0; i < 6; i++) {
-      // creates a table row
-      let row = document.createElement("tr");
-
-      //creating individual cells, filing them up with data.
-      for (let j = 0; j < 7; j++) {
-          if (i === 0 && j < firstDay) {
-              let cell = document.createElement("td");
-              let cellText = document.createTextNode("");
-              cell.appendChild(cellText);
-              row.appendChild(cell);
-          }  else if (date > daysInMonth) {
-              break;
-          } else {
-              let cell = document.createElement("td");
-              let cellText = document.createTextNode(date);
-              if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
-                  cell.classList.add("bg-info");
-              } // color today's date
-              cell.appendChild(cellText);
-              row.appendChild(cell);
-              date++;
-          }
-      }
-      tbl.appendChild(row); // appending each row into calendar body.
+    let firstDay = (new Date(year, month)).getDay();
+    let daysInMonth = 32 - new Date(year, month, 32).getDate();
+  
+    let tbl = document.getElementById("calendar-body"); // body of the calendar
+  
+    // clearing all previous cells
+    tbl.innerHTML = "";
+  
+    // filing data about month and in the page via DOM.
+    monthAndYear.innerHTML = months[month] + " " + year;
+    selectYear.value = year;
+    selectMonth.value = month;
+  
+    // creating all cells
+    let date = 1;
+    for (let i = 0; i < 6; i++) {
+        // creates a table row
+        let row = document.createElement("tr");
+  
+        //creating individual cells, filing them up with data.
+        for (let j = 0; j < 7; j++) {
+            if (i === 0 && j < firstDay) {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode("");
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }  else if (date > daysInMonth) {
+                break;
+            } else {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode(date);
+                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+                    cell.classList.add("bg-info");
+                } // color today's date
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                date++;
+            }
+        }
+        tbl.appendChild(row); // appending each row into calendar body.
+    }
   }
-}
+ }
 
+/*-----------------------------------------------------------*/
 // setting the characters limit to the message form on contact us page.
 function limitText(limitField, limitCount, limitNum) {
 	if (limitField.value.length > limitNum) {
